88394efecda7ad31def8429224183395bc92d88b
Thor Marius Henrichsen <thor.henrichsen@gmail.com>
2009-12-16 15:31:08 +0100

    Added ruby openid as plugin from aslakhellesoy instead of using gem version.

--- config/environment.rb
+++ config/environment.rb
@@ -20,12 +20,12 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
 
-  config.gem 'ruby-openid', :version => '2.1.7', :lib => 'openid'
+  #config.gem 'ruby-openid', :version => '2.1.7', :lib => 'openid'
   config.gem 'oauth', :version => '0.3.6'
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
-  #config.plugins = [ :exception_notification, :ssl_requirement, :all ]
+  config.plugins = ["ruby-openid".to_sym, :open_id_authentication, :all ]
 
   # Skip frameworks you're not going to use. To use Rails without a database,
   # you must remove the Active Record framework.
