a05df37b0dd4218c4cdbbfd9336ea22e9d19bb57
Thor Marius Henrichsen <thor.henrichsen@gmail.com>
2009-12-17 10:49:41 +0100

    removed emails view, rendering content-type: text/plain

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -2,6 +2,7 @@ class EmailsController < ApplicationController
   before_filter :login_required
 
   def index
+    headers["Content-Type"] = "text/plain"
     render :text => current_user.emails
   end
 
