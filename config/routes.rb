c887126f0609e886ae260572909680968c30b19d
Thor Marius Henrichsen <thor.henrichsen@gmail.com>
2009-12-17 15:29:04 +0100

    fixed route requirement

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ ActionController::Routing::Routes.draw do |map|
   map.root :controller => :sessions, :action => :new
   map.resource :session, :only => [:create, :destroy, :new]
   map.resources :emails, :only => [:index, :show]
-  map.resources :calendars, :only => [:index, :show]
+  map.resources :calendars, :only => [:index, :show], :requirements => { :id => /[a-zA-Z0-9\-\.\%]+/ }
   map.open_id_complete 'session', :controller => "sessions", :action => "create", :requirements => { :method => :get }
   map.authenticated_content '/authenticated', :controller => :authenticated, :action => :index
 end
