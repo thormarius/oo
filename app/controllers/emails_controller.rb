5d36e8dbc404414ec4097b4420acc6203dd63ea0
Thor Marius Henrichsen <thor.henrichsen@gmail.com>
2009-12-17 11:11:32 +0100

    pretty xml

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,7 +1,11 @@
+require 'rexml/document'
+
 class EmailsController < ApplicationController
   before_filter :login_required
 
-  def index; end
+  def index
+    @emails_atom = REXML::Document.new(current_user.emails).to_s(2)
+  end
 
 #  def show
 #    # Enkelt mail
