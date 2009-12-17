23b5d5b0813272786ba81529ba9025442f6a7862
Thor Marius Henrichsen <thor.henrichsen@gmail.com>
2009-12-17 14:10:21 +0100

    new .gems file

--- config/environment.rb
+++ config/environment.rb
@@ -21,9 +21,9 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
 
   #config.gem 'ruby-openid', :version => '2.1.7', :lib => 'openid'
-  config.gem 'oauth', :version => '0.3.6'
-  config.gem 'nokogiri', :version => '1.4.1'
-  
+  config.gem 'oauth',     :version => '0.3.6'
+  config.gem 'nokogiri',  :version => '1.4.1'
+  config.gem 'hpricot',   :version => '0.8.2'
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
