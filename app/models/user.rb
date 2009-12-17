b0527bac8b0b240a0e88ecc04df2efe2ae1d92bf
Thor Marius Henrichsen <thor.henrichsen@gmail.com>
2009-12-17 13:32:54 +0100

    added calendar links

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,15 @@ class User < ActiveRecord::Base
     event_feed(calendar).body
   end
 
+  def calendar_links
+    feed = calendars_feed
+    return [] unless Net::HTTPOK == feed
+    (Hpricot(feed.body) / "entry").map do |entry|
+      {
+        :url => (entry / "content[@type=application/atom+xml]").first.attributes['src'],
+        :name => (entry / "title").first
+      }
+    end
+  end
+
 end
